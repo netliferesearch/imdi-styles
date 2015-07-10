8f54264932dde8ab11d989e4cc67d8a2489e740e
Joakim Bording <joakim@netliferesearch.com>
2015-07-10 12:53:16 +0200

    Søk med filtrering og utlistingssider av forskjellige innholdstyper

--- Scripts/app.js
+++ Scripts/app.js
@@ -60,58 +60,62 @@ $( document ).ready(function() {
 	    TOGGLE - ACCESSIBLE
 	\*------------------------------------*/	
 	
-	$('.js-toggle').each(function() {
+	$('[data-behaviour="toggle"]').each(function() {
 	    // Hide sections on load
-	    var section = '#' + $(this).attr('aria-controls');
+	    var section = $(this).attr('data-controls');
 	    //var state = $(this).attr('aria-expanded') === 'false' ? true : false; 
 	    $(this).attr('aria-expanded','false');
-	    $(section).attr('aria-hidden','true');
-	    $(section).attr('tabindex','-1');
-	    $(section).hide();
+	    $(this).attr('aria-controls',section);
+	    $(this).attr('role','button');	    	    
+	    $('#' + section).attr('aria-hidden','true');
+	    $('#' + section).hide();
 	});
 	
-	$('.js-toggle').click(function() {
-	    // Set duration of animation
-	    var transition_duration = 250;
+	$('[data-behaviour="toggle"]').click(function() {
+	    // Set duration of animation in miliseconds
+	    var transition_duration = 200;
 	    
 	    // Get classes and relationships
-	    var toggle_class_open = $(this).data('open');
+	    var toggle_class_expanded = $(this).data('class-expanded');
 	    var section = '#' + $(this).attr('aria-controls');    
-	    var section_class_open = $(section).data('open');
-	    var section_caption_open = $(this).data('open_caption');
-	    var section_caption_close = $(this).data('close_caption');    
+	    var section_class_expanded = $(section).data('class-expanded');
+	    var section_class_contracted = $(section).data('class-contracted');	    
+	    var section_caption_expanded = $(this).data('caption-expanded');
+	    var section_caption_contracted = $(this).data('caption-contracted');    
 	    
 	    // Get current state
 	    var state = $(this).attr('aria-expanded') === 'false' ? true : false;    
 	    
-	    if(section_caption_close){
-		    if($(this).hasClass(toggle_class_open)){
-			    $(this).html(section_caption_close);
+	    if(section_caption_contracted){
+		    if($(this).hasClass(toggle_class_expanded)){
+			    $(this).html(section_caption_contracted);
 		    } else {
-			    $(this).html(section_caption_open);		    
+			    $(this).html(section_caption_expanded);		    
 		    }
 	    }    
 	    
 	    // Toggle attributes
 	    $(this).attr('aria-expanded', state);    
 	    $(section).attr('aria-hidden', !state);  
-	    $(this).toggleClass(toggle_class_open);
+	    $(this).toggleClass(toggle_class_expanded);
 	    if(state){ 
 	        // On opening
 	        $(section).toggle(0,function(){
 	            // Delay to be able to use css transitions and hide from screenreaders
-	            $(section).toggleClass(section_class_open); 
+   	            $(section).removeClass(section_class_contracted);
+	            $(section).addClass(section_class_expanded); 
 	           // $(section).focus();
 	        });
-	        if(section_caption_close){
-			    $(this).html(section_caption_close);
+	        if(section_caption_contracted){
+			    $(this).html(section_caption_contracted);
 		    }
 	    } else { 
 	        // On closing
-	        $(section).toggleClass(section_class_open);
+	        $(section).removeClass(section_class_expanded);
+	        $(section).addClass(section_class_contracted);	        
 	        setTimeout(function() { $(section).toggle(); }, transition_duration);
-	        if(section_caption_close){
-			    $(this).html(section_caption_open);
+	        if(section_caption_contracted){
+			    $(this).html(section_caption_expanded);
 		    }
 	    }
 	    event.preventDefault();
