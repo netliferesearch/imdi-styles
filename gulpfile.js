2ff4e3d60f3b9d265c5975f5b6c9ac3c6ea232cd
leifriksheim <riksheim.leif@gmail.com>
2018-06-12 10:26:42 +0200

    Remove public folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var minifycss = require('gulp-minify-css')
 var less = require('gulp-less')
 
 var package = require('./package.json');
-var version = package.version;
 
 gulp.task('styles', function () {
   gulp.src(['Styles/global/**/*.less'])
@@ -19,25 +18,15 @@ gulp.task('styles', function () {
       }}))
     .pipe(less())
     .pipe(autoprefixer('last 2 versions'))
-    .pipe(concat(`styles@${version}.css`))
-    .pipe(gulp.dest('public/'))
+    .pipe(concat(`styles.css`))
+    .pipe(gulp.dest('dist/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('public/'))
-})
+    .pipe(gulp.dest('dist/'));
 
-gulp.task('styles-test', function () {
-  gulp.src(['Styles/global/**/*.less'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message)
-        this.emit('end')
-      }}))
-    .pipe(less())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(concat(`styles-test.css`))
-    .pipe(gulp.dest('public/'))
-})
+  gulp.src(['README.md', 'package.json'])
+  .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('scripts', function () {
   return gulp.src('Scripts/app.js')
@@ -46,64 +35,14 @@ gulp.task('scripts', function () {
         console.log(error.message)
         this.emit('end')
       }}))
-    .pipe(concat(`app@${version}.js`))
-    .pipe(gulp.dest('public/'))
+    .pipe(concat(`app.js`))
+    .pipe(gulp.dest('dist/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('public/'))
-})
-
-gulp.task('scripts-test', function () {
-  return gulp.src('Scripts/app.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message)
-        this.emit('end')
-      }}))
-    .pipe(concat(`app-test.js`))
-    .pipe(gulp.dest('public/'))
-})
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('UI', function(){
   gulp.src('UI/**/*.*', { base: './' })
-  .pipe(gulp.dest('public/'));
+  .pipe(gulp.dest('dist/'));
 });
-
-gulp.task('UI-test', function(){
-  gulp.src('UI/**/*.*', { base: './' })
-  .pipe(concat(`UI-test`))
-  .pipe(gulp.dest('public/'));
-});
-
-// ==============================================
-//  UNCOMMENT BELOW IN DEVELOPMENT
-
-// var gulp = require('gulp');
-// var watchLess = require('gulp-watch-less2');
-// var watch = require('gulp-watch');
-// var less = require('gulp-less');
-// var concat = require('gulp-continuous-concat');
-// var uglify = require('gulp-uglify');
-// var uglifycss = require('gulp-uglifycss');
-//
-// var lessGlobs = 'Styles/global/**/*.less';
-// var jsGlobs = 'Scripts/app.js';
-//
-// gulp.task('styles', function () {
-//   return gulp.src(lessGlobs)
-//     .pipe(watchLess(lessGlobs, {verbose: true}))
-//     .pipe(less())
-//     .pipe(concat('styles.min.css'))
-//     .pipe(uglifycss())
-//     .pipe(gulp.dest('Styles/min/'))
-// });
-//
-// gulp.task('scripts', function () {
-//   return gulp.src(jsGlobs)
-//     .pipe(watch(jsGlobs, {verbose: true}))
-//     .pipe(concat('app.min.js'))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('Scripts/min/'));
-// });
-//
-// gulp.task('default', ['scripts', 'styles']);
